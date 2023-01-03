8dc48aae7d534c1c036fc0808ed3602ee971a170
shru271200 <sgupta4be19@gmail.com>
2023-01-03 18:13:19 +0000

    chatbot commit final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+import LoginForm from './Components/LoginForm';
+import ChatFeeds from './Components/ChatFeeds.jsx';
 import './App.css';
 
-function App() {
+const projectID = "fa87a416-d8e7-49fa-817a-95128efc941d";
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeeds {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
   );
-}
+};
 
 export default App;
